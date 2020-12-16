ce84e9b43972add298eafb83976b19bcbb164135
Syri <junimiso04@naver.com>
2020-12-17 00:58:33 +0900

    SplashWindow added.

--- source/MView/App.xaml.cs
+++ source/MView/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MView.Windows;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,35 @@ namespace MView
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //initialize the splash screen and set it as the application main window
+            var splashScreen = new SplashWindow();
+            this.MainWindow = splashScreen;
+            splashScreen.Show();
+
+            //in order to ensure the UI stays responsive, we need to
+            //do the work on a different thread
+            Task.Factory.StartNew(() =>
+            {
+                //simulate some work being done
+                System.Threading.Thread.Sleep(3000);
+
+                //since we're not on the UI thread
+                //once we're done we need to use the Dispatcher
+                //to create and show the main window
+                this.Dispatcher.Invoke(() =>
+                {
+                    //initialize the main window, set it as the application main window
+                    //and close the splash screen
+                    var mainWindow = new MainWindow();
+                    this.MainWindow = mainWindow;
+                    mainWindow.Show();
+                    splashScreen.Close();
+                });
+            });
+        }
     }
 }
