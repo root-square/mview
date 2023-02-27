eeba37cc60e9ba0e68f0053b5290664a813ae62d
June <hexstudio16@gmail.com>
2023-02-27 11:40:54 +0900

    feat: change explorer ui

---
+++ src/MView/Converters/FileNameToIconConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.IO;
+using MView.Utilities;
+
+namespace MView.Converters
+{
+    [ValueConversion(typeof(string), typeof(ImageSource))]
+    public class FileNameToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var fileName = (string)value;
+
+            if (File.Exists(fileName))
+            {
+                return IconManager.FindIconForFilename(fileName, false)!;
+            }
+            else
+            {
+                return new BitmapImage();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
